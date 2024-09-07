7e387808261c6a95f752cc5df6a823a1eb3bac9e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2024-09-06 22:37:47 -0400

    adding todo functionality

--- TodoWebApp/todowebapp/src/components/Todo.jsx
+++ TodoWebApp/todowebapp/src/components/Todo.jsx
@@ -1,7 +1,76 @@
+﻿import React, { useState } from 'react'
 function Todo() {
-  return (
-    <h2>TODO app contents</h2>
-  );
+    const [tasks, setTasks] = useState([
+        "Drink some coffee",
+        "Create a TODO app",
+        "Drink some more coffee"]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event) {
+        setNewTask(event.target.value);
+    }
+
+    function addTask() {
+        if (newTask.trim() !== "") {
+            setTasks(t => [...t, newTask]);
+            setNewTask("");
+        }
+    }
+
+    function deleteTask(index) {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+    }
+
+    function moveTaskUp(index) {
+        if (index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
+    function moveTaskDown(index) {
+        if (index < tasks.length) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
+    return (
+        <div>
+            <h1>TODO</h1>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter a task"
+                    value={newTask}
+                    onChange={handleInputChange} />
+                <button
+                    className="add-button"
+                    onClick={addTask}>
+                    Add
+                </button>
+            </div>
+            <ol>
+                {tasks.map((task, index) =>
+                    <li key={index}>
+                        <span className="text">{task}</span>
+                        <button onClick={()=>deleteTask(index)}>
+                            🗑️
+                        </button>
+                        <button onClick={() => moveTaskUp(index)}>
+                            ⇧
+                        </button>
+                        <button onClick={() => moveTaskDown(index)}>
+                            ⇩
+                        </button>
+                    </li>
+                )}
+            </ol>
+        </div>
+    );
 }
 
 export default Todo;
